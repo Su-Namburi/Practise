b8031b9da2f6398bb726313cca1de08f1c4e247a
Su-Namburi <su.namburi45@gmail.com>
2025-07-22 19:24:20 +0530

    test two

--- Arrays/src/NewArrays.java
+++ Arrays/src/NewArrays.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class NewArrays {
+    public static void main(String[] args) {
+        System.out.println("just a dmo to see if this get added to github");
+        System.out.println("lets see if i can add this");
+    }
 }
