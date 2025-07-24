323266c679919a112bd60094860ad1ccc70e90b3
Su-Namburi <su.namburi45@gmail.com>
2025-07-24 22:39:44 +0530

    bitwise

--- Arrays/src/NewArrays.java
+++ Arrays/src/NewArrays.java
@@ -1,6 +1,15 @@
 public class NewArrays {
     public static void main(String[] args) {
-        System.out.println("just a dmo to see if this get added to github");
-        System.out.println("lets see if i can add this");
+//        System.out.println("just a dmo to see if this get added to github");
+//        System.out.println("lets see if i can add this");
+
+        int a = -8;
+        System.out.println(a>>2);
+        System.out.println(a>>>2);
+//        System.out.println(a>>3);
+//        System.out.println("-------------------");
+//        System.out.println(a<<1);
+//        System.out.println(a<<2);
+//        System.out.println(a<<3);
     }
 }
