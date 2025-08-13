76503db1925d539bb982de196bde5c3a642bb849
Su-Namburi <su.namburi45@gmail.com>
2025-08-13 22:10:38 +0530

    trees

---
+++ Trees/src/Main.java
@@ -0,0 +1,19 @@
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.insert(8);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(15);
+        tree.insert(2);
+        tree.insert(5);
+
+        tree.inOrder();
+        System.out.println();
+        tree.preOrder();
+    }
+}
